d082a6c0f63f1ea37ed7b7b021f94c8c07ff234a
Raynnon <assante.f@gmail.com>
2022-07-27 23:54:10 +0200

    began to add social networks in ourTeam component

--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -2,6 +2,10 @@ import florianImage from '../images/florian.jpeg';
 import { Parallax } from 'react-parallax';
 import background from '../images/gray-bg.jpg';
 import Emergence from './animations/Emergence';
+import facebookIcon from '../images/social-networks/facebook-icon.svg';
+import instagramIcon from '../images/social-networks/instagram-icon.svg';
+import linkedinIcon from '../images/social-networks/linkedin-icon.svg';
+import twitterIcon from '../images/social-networks/twitter-icon.svg';
 
 interface Team {
   name: string;
@@ -12,28 +16,32 @@ interface Team {
 
 const team: Team[] = [
   {
-    name: "Emilia Noah",
-    role: "Front-End Developer",
+    name: 'Emilia Noah',
+    role: 'Front-End Developer',
     image: florianImage,
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores."
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores.'
   },
   {
-    name: "Emilia Noah",
-    role: "Front-End Developer",
+    name: 'Emilia Noah',
+    role: 'Front-End Developer',
     image: florianImage,
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores."
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores.'
   },
   {
-    name: "Emilia Noah",
-    role: "Front-End Developer",
+    name: 'Emilia Noah',
+    role: 'Front-End Developer',
     image: florianImage,
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores."
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores.'
   },
   {
-    name: "Emilia Noah",
-    role: "Front-End Developer",
+    name: 'Emilia Noah',
+    role: 'Front-End Developer',
     image: florianImage,
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores."
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio provident dolores.'
   }
 ];
 
@@ -47,21 +55,32 @@ export default function OurTeam() {
           </h2>
         </Emergence>
         <div className="cards">
-        {team.map((member, index) => (
-          <><div className="card" key={index}>
-          <img src={member.image} alt={member.name} />
-          <h2>{member.name}</h2>
-          <p className="role">{member.role}</p>
-          <p>{member.description}</p>
-          <div className="social-networks"></div>
-        </div>
-        
-        </>
-          
-        ))}
+          {team.map((member, index) => (
+            <div className="card">
+              <div className="card-infos" key={index}>
+                <img src={member.image} alt={member.name} />
+                <h2>{member.name}</h2>
+                <p className="role">{member.role}</p>
+                <p>{member.description}</p>
+              </div>
+              <div className="social-networks">
+                <a href="#">
+                  <img src={facebookIcon} alt="facebook-icon" />
+                </a>
+                <a href="#">
+                  <img src={twitterIcon} alt="facebook-icon" />
+                </a>
+                <a href="#">
+                  <img src={linkedinIcon} alt="facebook-icon" />
+                </a>
+                <a href="#">
+                  <img src={instagramIcon} alt="facebook-icon" />
+                </a>
+              </div>
+            </div>
+          ))}
         </div>
       </section>
     </Parallax>
-    
   );
 }

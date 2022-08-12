36d427d550fa67629c943ce8d7f8ba245120123c
Raynnon <assante.f@gmail.com>
2022-08-12 16:47:53 +0200

    finished section component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,26 +1,47 @@
-import Emergence from "./animations/Emergence";
-import Button from "./layout/Button";
+import Section from '../components/layout/Section';
+import Emergence from './animations/Emergence';
+import Button from './layout/Button';
 
 export default function OurTeam() {
   return (
-      <section id="contact">
-        <Emergence>
-          <h2>
-            Get <span>in</span> touch
-          </h2>
-        </Emergence>
-        
+    <Section title={['Get', 'in touch']} background="light">
+      <>
         <form>
-            <p>Lorem ipsum madolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor coli incidit labore lorem ipsum amet madolor sit amet.</p>
-            <div>
-                <input type="text" id="name" name="name" placeholder="Enter Your Name" />
-                <input type="text" id="email" name="email" placeholder="Enter Your Email" />
-                <input type="text" id="subject" name="subject" placeholder="Enter Your Subject" />
-            </div>
-            <textarea name="textarea" rows={4} cols={50} placeholder="Enter Your Message" />
+          <p>
+            Lorem ipsum madolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor coli incidit labore lorem ipsum amet madolor sit
+            amet.
+          </p>
+          <div>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter Your Name"
+            />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Enter Your Email"
+            />
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              placeholder="Enter Your Subject"
+            />
+          </div>
+          <textarea
+            name="textarea"
+            rows={4}
+            cols={50}
+            placeholder="Enter Your Message"
+          />
         </form>
 
         <Button text="Send message" color={true} />
-      </section>
+      </>
+    </Section>
   );
 }
--- src/components/OurTeam.tsx
+++ src/components/OurTeam.tsx
@@ -1,8 +1,5 @@
 import florianImage from '../images/florian.jpeg';
-import { Parallax } from 'react-parallax';
-import background from '../images/gray-bg.jpg';
 
-import Emergence from './animations/Emergence';
 import Section from '../components/layout/Section';
 
 import facebookIcon from '../images/social-networks/facebook-icon.svg';
@@ -50,40 +47,33 @@ const team: Team[] = [
 
 export default function OurTeam() {
   return (
-    <Parallax bgImage={background} strength={400}>
-      <section id="our-team">
-        <Emergence>
-          <h2>
-            Our <span>Team</span>
-          </h2>
-        </Emergence>
-        <div className="cards">
-          {team.map((member, index) => (
-            <div className="card">
-              <div className="card-infos" key={index}>
-                <img src={member.image} alt={member.name} />
-                <h2>{member.name}</h2>
-                <p className="role">{member.role}</p>
-                <p>{member.description}</p>
-              </div>
-              <div className="social-networks">
-                <a href="#">
-                  <img src={facebookIcon} alt="facebook-icon" />
-                </a>
-                <a href="#">
-                  <img src={twitterIcon} alt="facebook-icon" />
-                </a>
-                <a href="#">
-                  <img src={linkedinIcon} alt="facebook-icon" />
-                </a>
-                <a href="#">
-                  <img src={instagramIcon} alt="facebook-icon" />
-                </a>
-              </div>
+    <Section title={['Our', 'Team']} background="parallax">
+      <div className="cards">
+        {team.map((member, index) => (
+          <div className="card">
+            <div className="card-infos" key={index}>
+              <img src={member.image} alt={member.name} />
+              <h2>{member.name}</h2>
+              <p className="role">{member.role}</p>
+              <p>{member.description}</p>
             </div>
-          ))}
-        </div>
-      </section>
-    </Parallax>
+            <div className="social-networks">
+              <a href="#">
+                <img src={facebookIcon} alt="facebook-icon" />
+              </a>
+              <a href="#">
+                <img src={twitterIcon} alt="facebook-icon" />
+              </a>
+              <a href="#">
+                <img src={linkedinIcon} alt="facebook-icon" />
+              </a>
+              <a href="#">
+                <img src={instagramIcon} alt="facebook-icon" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Section>
   );
 }
